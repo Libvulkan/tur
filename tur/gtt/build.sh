3cbb6185a7b09e4038b98c8e0aaeaeb4b905bece
Libvulkan <161806671+Libvulkan@users.noreply.github.com>
2025-01-22 19:23:46 +0530

    Update build.sh

--- tur/gtt/build.sh
+++ tur/gtt/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="Android Vulkan wrapper"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="xMeM <haooy@outlook.com>"
-TERMUX_PKG_VERSION="25.0.0"
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/Libvulkan/mesa
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libx11, libxcb, libxshmfence, libwayland, vulkan-loader-generic, zlib, zstd"
